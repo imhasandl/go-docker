cd00b2798a635ac0706a5a32e9e36045b668bea5
Hasan <imhasandl04@gmail.com>
2024-12-13 18:33:59 +0500

    python docker container test and go docker container

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
+
 	m := http.NewServeMux()
 
 	m.HandleFunc("/", handlePage)
 
-	const addr = ":8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	addr := ":" + port
+
 	srv := http.Server{
 		Handler:      m,
 		Addr:         addr,
@@ -32,7 +45,7 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+	<p> Hello from Docker! I'm a Go server Hasan. </p>
 </body>
 </html>
 `
